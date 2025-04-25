5f360862070353468b603baaabdd7d5234f49c61
Ilya <sange0337@gmail.com>
2025-04-25 09:45:17 +0800

    replace the data location

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { answerDescriptions, Question, questions } from './data/questions';
+
 import { segmentMap } from './data/segmentMap';
-import { enrichedSegments, SegmentObject } from './data/segments';
 import './style.css';
 import { accessDomElement } from './utils/accessDomElement';
 import { accessDomElements } from './utils/accessDomElements';
@@ -263,9 +262,6 @@ export class QuizHandler {
     }
 }
 
-const quiz = new QuizHandler(questions, answerDescriptions);
-quiz.init();
-
 function createArrow(): HTMLDivElement {
     const wrapper = buildElement('div', 'arrow-wrapper');
     wrapper.innerHTML = `
@@ -275,3 +271,207 @@ function createArrow(): HTMLDivElement {
   </svg>`;
     return wrapper;
 }
+
+export const ANSWERS = {
+    BELOW_1_HOUR: { value: 1, label: 'Below 1 hours per week' },
+    HOURS_1_TO_3: { value: 2, label: '1-3 hours per week' },
+    ABOVE_4_HOURS: { value: 3, label: 'Above 4 hours per week' },
+    BELOW_70_KG: { value: 1, label: 'Below 70kg' },
+    ABOVE_80_KG: { value: 2, label: 'Above 80kg' },
+    COMFORT_PACE: { value: 1, label: 'Comfort pace' },
+    FAST_PACE: { value: 2, label: 'Fast pace' },
+    SEVERE_OVERPRONATION_YES: { value: 1, label: 'Yes' },
+    SEVERE_OVERPRONATION_NO: { value: 2, label: 'No' },
+} as const;
+
+export type AnswerOption = {
+    value: number;
+    label: string;
+};
+export type Question = {
+    id: number;
+    question: string;
+    answers: AnswerOption[];
+};
+
+export const questions: Question[] = [
+    {
+        id: 1,
+        question: 'How many hours of sports do you practice per week?',
+        answers: [
+            ANSWERS.BELOW_1_HOUR,
+            ANSWERS.HOURS_1_TO_3,
+            ANSWERS.ABOVE_4_HOURS,
+        ],
+    },
+    {
+        id: 2,
+        question: 'What is your weight?',
+        answers: [ANSWERS.BELOW_70_KG, ANSWERS.ABOVE_80_KG],
+    },
+    {
+        id: 3,
+        question: 'How do you plan to use your shoes?',
+        answers: [ANSWERS.COMFORT_PACE, ANSWERS.FAST_PACE],
+    },
+    {
+        id: 4,
+        question: 'Do you have severe overpronation?',
+        answers: [
+            ANSWERS.SEVERE_OVERPRONATION_YES,
+            ANSWERS.SEVERE_OVERPRONATION_NO,
+        ],
+    },
+];
+
+export const answerDescriptions: Record<string, string> = {
+    [ANSWERS.BELOW_1_HOUR.label]:
+        'Your stability muscles and ligaments are not yet developed. This means we should focus on shoes with average or increased stability.',
+    [ANSWERS.HOURS_1_TO_3.label]:
+        'Your stability muscles and ligaments are developed enough to use shoes with average or slightly increased stability. However, highly unstable shoes are still risky.',
+    [ANSWERS.ABOVE_4_HOURS.label]:
+        'Your stability muscles and ligaments are well prepared to handle any shoes. Therefore, we can focus on faster models, although they offer lower stability.',
+    [ANSWERS.BELOW_70_KG.label]:
+        'Your weight is relatively low, so we recommend lighter shoes. However, they will provide less cushioning.',
+    [ANSWERS.ABOVE_80_KG.label]:
+        'You are above average weight, so we suggest choosing shoes with increased cushioning.',
+    [ANSWERS.COMFORT_PACE.label]:
+        'We prioritize shoes that offer maximum comfort and safety.',
+    [ANSWERS.FAST_PACE.label]:
+        'We prioritize shoes that enhance your running speed.',
+    [ANSWERS.SEVERE_OVERPRONATION_YES.label]:
+        'Severe overpronation increases instability, so you should avoid unstable shoes (this applies only to untrained runners).',
+    [ANSWERS.SEVERE_OVERPRONATION_NO.label]:
+        'Without overpronation problems, you have no specific limitations.',
+};
+
+const quiz = new QuizHandler(questions, answerDescriptions);
+quiz.init();
+
+export type SegmentObject = {
+    id: number;
+    name: string;
+    description: string;
+    shoes: string[];
+  };
+
+export const enrichedSegments: SegmentObject[] = [
+    {
+        id: 1,
+        name: 'Stability',
+        description: 'Offers exceptional lightness and foot support.',
+        shoes: ['Sprint Edge', 'Runner Pro', 'CushionX'],
+    },
+    {
+        id: 2,
+        name: 'Stability with extra cushioning',
+        description: 'Perfect for long distances with enhanced cushioning.',
+        shoes: ['GlideForm', 'AirFlow Max'],
+    },
+    {
+        id: 3,
+        name: 'Stability with extra lightness',
+        description: 'Recommended for daily training with great lightness.',
+        shoes: ['TrailBlazer', 'Speedster', 'CushionX'],
+    },
+    {
+        id: 4,
+        name: 'Support',
+        description:
+            'Ideal for cushioning runners looking for balance and performance.',
+        shoes: ['Runner Pro', 'GlideForm'],
+    },
+    {
+        id: 5,
+        name: 'Support with extra cushioning',
+        description: 'Designed for maximum stability and style.',
+        shoes: ['TrailBlazer', 'AirFlow Max', 'Sprint Edge'],
+    },
+    {
+        id: 6,
+        name: 'Support with extra lightness',
+        description: 'Perfect for long distances with enhanced stability.',
+        shoes: ['Speedster', 'CushionX'],
+    },
+    {
+        id: 7,
+        name: 'Balance',
+        description: 'Recommended for daily training with great grip.',
+        shoes: ['AirFlow Max', 'Runner Pro'],
+    },
+    {
+        id: 8,
+        name: 'Balance with extra cushioning',
+        description:
+            'Ideal for cushioning runners looking for balance and performance.',
+        shoes: ['TrailBlazer', 'Sprint Edge'],
+    },
+    {
+        id: 9,
+        name: 'Balance with extra lightness',
+        description: 'Offers exceptional cushioning and foot support.',
+        shoes: ['Speedster', 'CushionX', 'GlideForm'],
+    },
+    {
+        id: 10,
+        name: 'Swing',
+        description: 'Perfect for long distances with enhanced grip.',
+        shoes: ['GlideForm', 'Runner Pro'],
+    },
+    {
+        id: 11,
+        name: 'Swing with extra cushioning',
+        description: 'Recommended for daily training with great comfort.',
+        shoes: ['Sprint Edge', 'TrailBlazer'],
+    },
+    {
+        id: 12,
+        name: 'Swing with extra lightness',
+        description: 'Offers exceptional grip and foot support.',
+        shoes: ['AirFlow Max', 'CushionX'],
+    },
+    {
+        id: 13,
+        name: 'Bounce',
+        description: 'Perfect for long distances with enhanced lightness.',
+        shoes: ['Speedster', 'GlideForm'],
+    },
+    {
+        id: 14,
+        name: 'Bounce with extra cushioning',
+        description:
+            'Ideal for stability runners looking for balance and performance.',
+        shoes: ['Sprint Edge', 'AirFlow Max', 'Runner Pro'],
+    },
+    {
+        id: 15,
+        name: 'Bounce with extra lightness',
+        description: 'Recommended for daily training with great cushioning.',
+        shoes: ['TrailBlazer', 'Speedster'],
+    },
+    {
+        id: 16,
+        name: 'Speed illegal',
+        description: 'Designed for maximum comfort and style.',
+        shoes: ['CushionX', 'Runner Pro', 'Sprint Edge'],
+    },
+    {
+        id: 17,
+        name: 'Speed Swing',
+        description: 'Perfect for long distances with enhanced comfort.',
+        shoes: ['Speedster', 'AirFlow Max'],
+    },
+    {
+        id: 18,
+        name: 'Speed lightness',
+        description: 'Offers exceptional grip and foot support.',
+        shoes: ['TrailBlazer', 'GlideForm'],
+    },
+    {
+        id: 19,
+        name: 'Speed lightness bouncy',
+        description:
+            'Ideal for grip runners looking for balance and performance.',
+        shoes: ['Runner Pro', 'CushionX'],
+    },
+];
